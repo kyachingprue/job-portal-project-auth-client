a84f1f499eb04db2fa5a893c9493b010b8f0d075
kyachingprue <kyachingpruemarma.info@gmail.com>
2025-07-27 12:01:44 +0600

    new update commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 3000, // default is 500 (in KB)
+  },
   plugins: [tailwindcss(), react()],
 });
